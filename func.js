5a9f38bcc8b5fdfe27a3d868548a7fb87a5dfbb7
Alexandre Banzato Dorneles <alexandre.ads.2710@gmail.com>
2023-03-07 09:40:27 -0400

    primeiro commit, apenas algumas alterações

--- func.js
+++ func.js
@@ -31,7 +31,7 @@ function menosfunc(){
 
 
 
-var img1 = "./Assets/imgs/pedraFilosofal.jpg";
+var img1 = "./Assets/imgs/pedraFilosofal.jpg"
 var img2 = "./Assets/imgs/camaraSecreta.jpg"
 var img3 = "./Assets/imgs/prisioneirodeaskaban.jpg"
 var img4 = "./Assets/imgs/caliseDeFogo.jpg"
@@ -40,33 +40,45 @@ var img6 = "./Assets/imgs/eni.jpg"
 var img7 = "./Assets/imgs/reliquiasDaMorte.jpg"
 
 
-var imgArea = "document.getElementById('img')"
+var imgArea = document.getElementById('img')
+ 
+var audio = document.getElementById('BookAudio')
+    
 
 function harryPotter01() {
     imgArea =  document.getElementById('img').src = img1 ;
+    audio.play();
 }
 
 function harryPotter02(){
     imgArea =  document.getElementById('img').src = img2;
-
+    audio.play();
+    
 }
 function harryPotter03(){
     imgArea =  document.getElementById('img').src = img3;
+    audio.play();
 
 }
 function harryPotter04(){
-    imgArea =  document.getElementById('img').src = img4
+    imgArea =  document.getElementById('img').src = img4;
+    audio.play();
 
 }
 function harryPotter05(){
     imgArea =  document.getElementById('img').src = img5;
+    audio.play();
 
 }
 function harryPotter06(){
     imgArea =  document.getElementById('img').src = img6;
+    audio.play();
 
 }
 function harryPotter07(){
     imgArea =  document.getElementById('img').src = img7;
+    audio.play();
 
 }
+
+
